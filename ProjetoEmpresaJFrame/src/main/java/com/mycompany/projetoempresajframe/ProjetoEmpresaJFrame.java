808004e222e09e9b7cd46a9bd3961ed4e4769015
Oysterfield <116518676+Oysterfield@users.noreply.github.com>
2024-09-24 14:09:01 -0300

    Add files

---
+++ ProjetoEmpresaJFrame/src/main/java/com/mycompany/projetoempresajframe/ProjetoEmpresaJFrame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetoempresajframe;
+
+/**
+ *
+ * @author Win10
+ */
+public class ProjetoEmpresaJFrame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
