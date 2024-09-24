2b52274ac5cff944a2c16895156098220b0d20dd
Oysterfield <116518676+Oysterfield@users.noreply.github.com>
2024-09-24 14:00:51 -0300

    Add files

---
+++ Banco/src/main/java/com/mycompany/banco/Transferencias.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.banco;
+
+/**
+ *
+ * @author Win10
+ */
+public class Transferencias extends Transacoes {
+    public String transacao() {}
+}
