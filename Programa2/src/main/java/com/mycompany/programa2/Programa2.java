a7a0e84ed290e4bbcc4cdb26262c837257c796f9
Oysterfield <116518676+Oysterfield@users.noreply.github.com>
2024-09-24 14:08:24 -0300

    Add files

---
+++ Programa2/src/main/java/com/mycompany/programa2/Programa2.java
@@ -0,0 +1,35 @@
+package com.mycompany.programa2;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+/**
+ *
+ * @author Win10
+ */
+public class Programa2 {
+   
+    
+    public static void main(String[] args) {
+        
+        int x = 10;
+        int y = 10;
+        
+        if (x < 10)
+            if (y > 10)
+                System.out.println("*****");
+            else
+                System.out.println("#####");
+        System.out.println("$");
+        if (x < 10) {
+            if (y > 10)
+                System.out.println("*****");
+            }
+        else {
+            System.out.println("#####");
+            System.out.println("$");
+            }
+    }
+}
