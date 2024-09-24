c27c8024bb41b3d3530639674b9454c51fb9eb08
Oysterfield <116518676+Oysterfield@users.noreply.github.com>
2024-09-24 14:00:06 -0300

    Add files

---
+++ AtivPrat4_Cancian_Molde/Atividade Prática 4 -- prática em aula e remota -- 23_05 -- Serialização de objetos/Gerenciamento_Empresas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ufsc.ctc.ine.ine5404.gerenciamento_empresas.model;
+
+/**
+ *
+ * @author cancian
+ */
+public class Gerenciamento_Empresas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
