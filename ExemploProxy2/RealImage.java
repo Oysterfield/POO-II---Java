6ce63d161b41c5d7caba951090b0e0c410ed37b0
Oysterfield <116518676+Oysterfield@users.noreply.github.com>
2024-09-24 14:03:17 -0300

    Add files

---
+++ ExemploProxy2/RealImage.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package designpatternsexamples.Proxy;
+
+/**
+ <p>
+ @author developer
+ */
+
+//on System A 
+public class RealImage implements Image {
+ 
+    private String filename = null;
+    /**
+     * Constructor
+     * @param filename
+     */
+    public RealImage(final String filename) { 
+        this.filename = filename;
+        loadImageFromDisk();
+    }
+ 
+    /**
+     * Loads the image from the disk
+     */
+    private void loadImageFromDisk() {
+        System.out.println("Loading   " + filename);
+    }
+ 
+    /**
+     * Displays the image
+     */
+    public void displayImage() { 
+        System.out.println("Displaying " + filename); 
+    }
+ 
+}
