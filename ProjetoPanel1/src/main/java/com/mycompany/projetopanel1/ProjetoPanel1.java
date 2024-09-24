3124a303f489d292b1c726b693e8764625545521
Oysterfield <116518676+Oysterfield@users.noreply.github.com>
2024-09-24 14:09:44 -0300

    Add files via upload

---
+++ ProjetoPanel1/src/main/java/com/mycompany/projetopanel1/ProjetoPanel1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetopanel1;
+
+/**
+ *
+ * @author Win10
+ */
+public class ProjetoPanel1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
